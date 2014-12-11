8a3a2c077fe52135805a51a4cced3b600f4d60ef
Joshua Estrin Skrzypek <jskrzypek@gmail.com>
2014-12-11 14:58:50 +0200

    test: Completed test suite for targets.js and fixed bug in targets to enable testing of cl params

--- test/mocha/targets.test.js
+++ test/mocha/targets.test.js
@@ -39,7 +39,7 @@ describe('targets', function() {
         // });
 
         it('should find all targets when required with only the clientFolder', function() {
-            var targets = require('../../lib/targets')(clientFolder, null, null);
+            var targets = require('../../lib/targets')(clientFolder);
 
             var expected = {
                 allTargets: ['app', 'web', 'mobile'],
@@ -58,5 +58,98 @@ describe('targets', function() {
             assert.deepEqual(_(targets.templates).sortBy(function(temp) { return temp.targetName; }).value(),
                         _(expected.templates).sortBy(function(temp) { return temp.targetName; }).value());
         });
+
+        it('should change defaultTarget name when provided', function() {
+            var targets = require('../../lib/targets')(clientFolder, 'base');
+
+            var expected = {
+                allTargets: ['base', 'web', 'mobile'],
+                names: ['base', 'web', 'mobile'],
+                suffixes: ['', '-web', '-mobile'],
+                templates: [
+                    {targetName: 'base', targetSuffix: ''},
+                    {targetName: 'web', targetSuffix: '-web'},
+                    {targetName: 'mobile', targetSuffix: '-mobile'}
+                ]
+            };
+
+            assert.deepEqual(_(targets.allTargets).sortBy().value(), _(expected.allTargets).sortBy().value());
+            assert.deepEqual(_(targets.names).sortBy().value(), _(expected.names).sortBy().value());
+            assert.deepEqual(_(targets.suffixes).sortBy().value(), _(expected.suffixes).sortBy().value());
+            assert.deepEqual(_(targets.templates).sortBy(function(temp) { return temp.targetName; }).value(),
+                        _(expected.templates).sortBy(function(temp) { return temp.targetName; }).value());
+        });
+
+        it('should return only one target when specified', function() {
+            var targets = require('../../lib/targets')(clientFolder, 'base', {target: 'web'});
+
+            var expected = {
+                allTargets: ['base', 'web', 'mobile'],
+                names: ['web'],
+                suffixes: ['-web'],
+                templates: [{targetName: 'web', targetSuffix: '-web'}]
+            };
+
+            assert.deepEqual(_(targets.allTargets).sortBy().value(), _(expected.allTargets).sortBy().value());
+            assert.deepEqual(_(targets.names).sortBy().value(), _(expected.names).sortBy().value());
+            assert.deepEqual(_(targets.suffixes).sortBy().value(), _(expected.suffixes).sortBy().value());
+            assert.deepEqual(_(targets.templates).sortBy(function(temp) { return temp.targetName; }).value(),
+                        _(expected.templates).sortBy(function(temp) { return temp.targetName; }).value());
+        });
+
+        it('should return only the targets specified', function() {
+            var targets = require('../../lib/targets')(clientFolder, 'base', {target: ['web', 'mobile']});
+
+            var expected = {
+                allTargets: ['base', 'web', 'mobile'],
+                names: ['web', 'mobile'],
+                suffixes: ['-web', '-mobile'],
+                templates: [
+                    {targetName: 'web', targetSuffix: '-web'},
+                    {targetName: 'mobile', targetSuffix: '-mobile'}
+                ]
+            };
+
+            assert.deepEqual(_(targets.allTargets).sortBy().value(), _(expected.allTargets).sortBy().value());
+            assert.deepEqual(_(targets.names).sortBy().value(), _(expected.names).sortBy().value());
+            assert.deepEqual(_(targets.suffixes).sortBy().value(), _(expected.suffixes).sortBy().value());
+            assert.deepEqual(_(targets.templates).sortBy(function(temp) { return temp.targetName; }).value(),
+                        _(expected.templates).sortBy(function(temp) { return temp.targetName; }).value());
+        });
+
+        it('should return the defaultTarget when specified', function() {
+            var targets = require('../../lib/targets')(clientFolder, 'base', {target: ['base', 'mobile']});
+
+            var expected = {
+                allTargets: ['base', 'web', 'mobile'],
+                names: ['base', 'mobile'],
+                suffixes: ['', '-mobile'],
+                templates: [
+                    {targetName: 'base', targetSuffix: ''},
+                    {targetName: 'mobile', targetSuffix: '-mobile'}
+                ]
+            };
+
+            assert.deepEqual(_(targets.allTargets).sortBy().value(), _(expected.allTargets).sortBy().value());
+            assert.deepEqual(_(targets.names).sortBy().value(), _(expected.names).sortBy().value());
+            assert.deepEqual(_(targets.suffixes).sortBy().value(), _(expected.suffixes).sortBy().value());
+            assert.deepEqual(_(targets.templates).sortBy(function(temp) { return temp.targetName; }).value(),
+                        _(expected.templates).sortBy(function(temp) { return temp.targetName; }).value());
+        });
+
+        it('should throw when an invalid target is specified', function() {
+            assert.throws(function() {
+                require('../../lib/targets')(clientFolder, 'base', {target: ['base', 'bo']});
+            }, Error);
+
+            assert.throws(function() {
+                require('../../lib/targets')(clientFolder, 'base', {target: ['base', 'bo']});
+            }, Error);
+
+            assert.throws(function() {
+                require('../../lib/targets')(clientFolder, 'base', {target: 'app'});
+            }, Error);
+        });
+
     });
 });

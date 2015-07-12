87b438914ae037887e5fe65a38288b83507c9259
Avi Haiat <thaiat@ipelia.fr>
2015-07-12 15:41:11 +0300

    feat(gulps): Modify changelog task

--- gulp_tasks/common/helper.js
+++ gulp_tasks/common/helper.js
@@ -1,9 +1,11 @@
 'use strict';
 
+var gulp = require('gulp');
 var fs = require('fs');
 var gutil = require('gulp-util');
 var stripJsonComments = require('strip-json-comments');
 var _ = require('lodash');
+var es = require('event-stream');
 var path = require('path');
 
 /**
@@ -50,10 +52,24 @@ var filterFiles = function(files, extension) {
     });
 };
 
+/**
+ * Add new sources in a gulp pipeline
+ * @returns {Stream} A gulp stream
+ * @example
+ * gulp.src('')
+ * .pipe(addSrc('CHANGELOG.md'))
+ * .gulp.dest();
+ */
+var addSrc = function() {
+    var pass = es.through();
+    return es.duplex(pass, es.merge(gulp.src.apply(gulp.src, arguments), pass));
+};
+
 module.exports = {
     isMobile: isMobile,
     execHandler: execHandler,
     readTextFile: readTextFile,
     readJsonFile: readJsonFile,
-    filterFiles: filterFiles
+    filterFiles: filterFiles,
+    addSrc: addSrc
 };
